29a0ca952718fc4f9fca4628530d5f15c1b2e65e
kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
2024-05-05 22:01:32 +0700

    Back-end Updates

--- backend/modules/passpost.js
+++ backend/modules/passpost.js
@@ -2,7 +2,6 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const User = require('../models/user.model')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const Payment = require('../models/payment.model')
 const dotenv = require('dotenv')
 const path = require('path');
 const https = require('https');
